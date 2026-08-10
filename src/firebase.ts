0de7dc5c2664a0f32b6de7e81a6e4fe9633155e1
jhosstynn13-png <jhosstynn13@gmail.com>
2026-08-10 16:44:59 -0500

    v1

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,5 +4,5 @@ import { getAuth } from 'firebase/auth';
 import config from '../firebase-applet-config.json';
 
 const app = initializeApp(config);
-export const db = getFirestore(app);
+export const db = getFirestore(app, config.firestoreDatabaseId);
 export const auth = getAuth(app);
